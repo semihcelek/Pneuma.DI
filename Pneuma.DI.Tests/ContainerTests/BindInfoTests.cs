01fec28766e2848917c4fbcf0d7d006a3b704ccc
Semih Celek <cybermoon1999@gmail.com>
2023-02-19 00:06:21 +0300

    update binding tests

--- Pneuma.DI.Tests/ContainerTests/BindInfoTests.cs
+++ Pneuma.DI.Tests/ContainerTests/BindInfoTests.cs
@@ -1,28 +1,22 @@
-﻿using NUnit.Framework;
-using Pneuma.DI.Core;
+﻿using System;
+using NUnit.Framework;
+using Pneuma.DI.Core.BindingContexts;
+using Pneuma.DI.Core.Bindings;
 using Pneuma.DI.Tests.Examples;
 
 namespace Pneuma.DI.Tests.ContainerTests
 {
     public class BindInfoTests
     {
-        // [Test]
-        // public void Binding_Retrieves_Type()
-        // {
-        //     Foo fooInstance = new Foo();
-        //     Binding binding = new Binding(fooInstance, fooInstance.GetType());
-        //     
-        //     Assert.IsTrue(typeof(Foo) == binding.GetBindingType());
-        //     Assert.IsAssignableFrom<Foo>(binding.GetBindingInstance());
-        // }
-        //
-        // [Test]
-        // public void Binding_HashCode_Same_As_Injected_Objects_HashCode()
-        // {
-        //     Foo fooInstance = new Foo();
-        //     Binding binding = new Binding(fooInstance, fooInstance.GetType());
-        //
-        //     Assert.AreEqual(fooInstance.GetHashCode(), binding.GetHashCode());
-        // }
+        [Test]
+        public void Binding_Retrieves_Type()
+        {
+            Foo fooInstance = new Foo();
+            Type bindingType = fooInstance.GetType();
+            Binding binding = new Binding(fooInstance, bindingType, bindingType, BindingLifeTime.Singular);
+            
+            Assert.IsTrue(typeof(Foo) == binding.BindingType);
+            Assert.IsAssignableFrom<Foo>(binding.Instance);
+        }
     }
 }
